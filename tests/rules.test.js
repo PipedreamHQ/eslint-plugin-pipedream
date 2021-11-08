dfaf3d2740b61f7b5a7d7ec29a4d04ac5c4d9eae
js07 <19861096+js07@users.noreply.github.com>
2021-11-08 14:29:03 -0500

    Add test cases for lines above export default declaration

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -31,6 +31,13 @@ function convertObjectToESMExportString(obj) {
   return `export default ${JSON.stringify(obj)}`;
 }
 
+function withPrecedingStatement(code) {
+  return `
+  import foo from "bar";
+  ${code}
+  `;
+}
+
 ruleTester.run("required-properties-key-test", rules["required-properties-key"], {
   valid: [
     {
@@ -348,4 +355,355 @@ ruleTester.run("ts-version-test", rules["no-ts-version"], {
       ],
     },
   ],
+});
+
+RuleTester.describe("On ESM export default with preceding statements", () => {
+  ruleTester.run("required-properties-key-test", rules["required-properties-key"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(requiredPropertyKeyMissing)),
+        errors: [
+          {
+            message: "Components must export a key property. See https://pipedream.com/docs/components/guidelines/#required-metadata",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("required-properties-key-test", rules["required-properties-key"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(requiredPropertyKeyMissing)),
+        errors: [
+          {
+            message: "Components must export a key property. See https://pipedream.com/docs/components/guidelines/#required-metadata",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(requiredPropertyKeyMissing)),
+        errors: [
+          {
+            message: "Components must export a key property. See https://pipedream.com/docs/components/guidelines/#required-metadata",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("required-properties-name-test", rules["required-properties-name"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(requiredPropertyNameMissing)),
+        errors: [
+          {
+            message: "Components must export a name property. See https://pipedream.com/docs/components/guidelines/#required-metadata",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(requiredPropertyNameMissing)),
+        errors: [
+          {
+            message: "Components must export a name property. See https://pipedream.com/docs/components/guidelines/#required-metadata",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("required-properties-description-test", rules["required-properties-description"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(
+          convertObjectToCJSExportString(requiredPropertyDescriptionMissing),
+        ),
+        errors: [
+          {
+            message: "Components must export a description property. See https://pipedream.com/docs/components/guidelines/#required-metadata",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(
+          convertObjectToESMExportString(requiredPropertyDescriptionMissing),
+        ),
+        errors: [
+          {
+            message: "Components must export a description property. See https://pipedream.com/docs/components/guidelines/#required-metadata",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("required-properties-version-test", rules["required-properties-version"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(
+          convertObjectToCJSExportString(requiredPropertyVersionMissing),
+        ),
+        errors: [
+          {
+            message: "Components must export a version property. See https://pipedream.com/docs/components/guidelines/#required-metadata",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(
+          convertObjectToESMExportString(requiredPropertyVersionMissing),
+        ),
+        errors: [
+          {
+            message: "Components must export a version property. See https://pipedream.com/docs/components/guidelines/#required-metadata",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("required-properties-type-test", rules["required-properties-type"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(requiredPropertyTypeMissing)),
+        errors: [
+          {
+            message: "Components must export a type property (\"source\" or \"action\")",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(requiredPropertyTypeMissing)),
+        errors: [
+          {
+            message: "Components must export a type property (\"source\" or \"action\")",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("default-value-required-for-optional-props-test", rules["default-value-required-for-optional-props"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(
+          convertObjectToCJSExportString(optionalPropWithoutDefaultValue),
+        ),
+        errors: [
+          {
+            message: "Component prop test is marked \"optional\", so it may need a \"default\" property. See https://pipedream.com/docs/components/guidelines/#default-values",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(
+          convertObjectToESMExportString(optionalPropWithoutDefaultValue),
+        ),
+        errors: [
+          {
+            message: "Component prop test is marked \"optional\", so it may need a \"default\" property. See https://pipedream.com/docs/components/guidelines/#default-values",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("props-label-test", rules["props-label"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(missingPropsLabel)),
+        errors: [
+          {
+            message: "Component prop test must have a label. See https://pipedream.com/docs/components/guidelines/#props",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(missingPropsLabel)),
+        errors: [
+          {
+            message: "Component prop test must have a label. See https://pipedream.com/docs/components/guidelines/#props",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("props-description-test", rules["props-description"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(missingPropsDescription)),
+        errors: [
+          {
+            message: "Component prop test must have a description. See https://pipedream.com/docs/components/guidelines/#props",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(missingPropsDescription)),
+        errors: [
+          {
+            message: "Component prop test must have a description. See https://pipedream.com/docs/components/guidelines/#props",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("source-name-test", rules["source-name"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(badSourceName)),
+        errors: [
+          {
+            message: "Source names should start with \"New\". See https://pipedream.com/docs/components/guidelines/#source-name",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(badSourceName)),
+        errors: [
+          {
+            message: "Source names should start with \"New\". See https://pipedream.com/docs/components/guidelines/#source-name",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("source-description-test", rules["source-description"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(badSourceDescription)),
+        errors: [
+          {
+            message: "Source descriptions should start with \"Emit new\". See https://pipedream.com/docs/components/guidelines/#source-description",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(badSourceDescription)),
+        errors: [
+          {
+            message: "Source descriptions should start with \"Emit new\". See https://pipedream.com/docs/components/guidelines/#source-description",
+          },
+        ],
+      },
+    ],
+  });
+
+  ruleTester.run("ts-version-test", rules["no-ts-version"], {
+    valid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(valid)),
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(valid)),
+      },
+    ],
+    invalid: [
+      {
+        code: withPrecedingStatement(convertObjectToCJSExportString(tsVersion)),
+        errors: [
+          {
+            message: "{{ts}} macro should be removed before committing",
+          },
+        ],
+      },
+      {
+        code: withPrecedingStatement(convertObjectToESMExportString(tsVersion)),
+        errors: [
+          {
+            message: "{{ts}} macro should be removed before committing",
+          },
+        ],
+      },
+    ],
+  });
 });
